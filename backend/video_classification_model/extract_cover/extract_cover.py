2f8f603ce8235777e863bedf33f15316bd6ebd72
Felix Gan <havenville@PenguinStack.local>
2024-03-18 23:38:50 +0800

    adjusted requirements to use urllib3 < 2.0 and aded some cover extraction code but not finished yet

---
+++ backend/video_classification_model/extract_cover/extract_cover.py
@@ -0,0 +1,15 @@
+import os
+from os.path import join, basename, isfile
+
+cur_directory = os.path.dirname(os.path.realpath(__file__))
+
+input_folder = f"{cur_directory}/input_videos"
+output_folder = f"{cur_directory}/csv_outputs"
+
+for filename in os.listdir(input_folder):
+    in_filepath = join(input_folder, filename)
+    # checking if it is a file
+    if isfile(in_filepath):
+        out_file_name = basename(filename)
+        os.system(f"python3 -m ffmpeg_bitrate_stats  -a time -c 30 -of csv {input_folder}/{out_file_name}")
+    
